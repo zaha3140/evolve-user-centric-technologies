e49309aae4abc884793b0d08b7e4d2260df98598
zaha3140 <146467052+zaha3140@users.noreply.github.com>
2024-02-17 10:32:22 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,27 @@
+    for item in data:
+    data = generate_random_data()
+def main():
+if __name__ == "__main__":
+    main()
+
+
+import random
+
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+    return data
+        print(f"Random Number: {item}")
+
+def main():
+    for item in data:
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+import random
+    data = generate_random_data()
+def generate_random_data():
+
+    main()
